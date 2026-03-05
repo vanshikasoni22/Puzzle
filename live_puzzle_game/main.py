a62ec0bdcd00f53b4844666f9ab3c82c37f99894
vanshikasoni22 <vanshikasoni990@email.com>
2026-03-05 15:06:54 +0530

    puzzle

--- live_puzzle_game/main.py
+++ live_puzzle_game/main.py
@@ -5,6 +5,8 @@ cap = cv2.VideoCapture(0)
 
 detector = HandTracker()
 
+puzzle_image = None
+
 while True:
 
     success, img = cap.read()
@@ -16,13 +18,63 @@ while True:
     if landmark_list:
 
         x, y = landmark_list[8][1], landmark_list[8][2]
-
         cv2.circle(img, (x, y), 15, (255, 0, 255), cv2.FILLED)
 
-    cv2.imshow("Hand Tracking", img)
+    cv2.imshow("Camera", img)
 
-    if cv2.waitKey(1) & 0xFF == 27:
+    key = cv2.waitKey(1)
+
+    # press C to capture puzzle image
+    if key == ord('c'):
+        puzzle_image = img.copy()
+        break
+
+    if key == 27:
         break
 
 cap.release()
 cv2.destroyAllWindows()
+
+rows = 3
+cols = 3
+
+h, w, _ = puzzle_image.shape
+
+tile_h = h // rows
+tile_w = w // cols
+
+tiles = []
+# spliting image into tiles
+for r in range(rows):
+    for c in range(cols):
+
+        tile = puzzle_image[
+            r * tile_h:(r + 1) * tile_h,
+            c * tile_w:(c + 1) * tile_w
+        ]
+
+        tiles.append(tile)
+
+
+import random
+
+random.shuffle(tiles)
+
+
+# displaying puzzle
+puzzle = puzzle_image.copy()
+
+index = 0
+
+for r in range(rows):
+    for c in range(cols):
+
+        puzzle[
+            r * tile_h:(r + 1) * tile_h,
+            c * tile_w:(c + 1) * tile_w
+        ] = tiles[index]
+
+        index += 1
+
+cv2.imshow("Puzzle", puzzle)
+cv2.waitKey(0)
